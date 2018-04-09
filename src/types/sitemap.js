4e100e117fa9e483f9610f70f35988acf3860740
supernerbd <supernerbd@googlemail.com>
2018-04-09 14:49:22 +0200

    start refactor formatting

--- src/types/sitemap.js
+++ src/types/sitemap.js
@@ -62,3 +62,55 @@ export type RawNewsSiteMapData = {
 		stock_tickers?: string | string[],
 	},
 };
+
+export type MappedSiteMapData = {
+	// details read: https://developers.google.com/webmasters/videosearch/sitemaps
+	loc: string,
+	lastmod?: string,
+	priority?: number,
+	changefreq?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never',
+	'image:image'?: {
+		'image:loc': string,
+		'image:caption'?: string,
+		'image:geo_location'?: string,
+		'image:title'?: string,
+		'image:license'?: string,
+	}[],
+	'video:video'?: {
+		'video:thumbnail_loc': string,
+		'video:title': string,
+		'video:description': string,
+		'video:content_loc'?: string,
+		'video:player_loc'?: { '#text': string, '@autoplay'?: string },
+		'video:duration'?: string,
+		'video:expiration_date'?: string,
+		'video:rating'?: number,
+		'video:view_count'?: number,
+		'video:publication_date'?: string,
+		'video:family_friendly'?: string,
+		'video:tag'?: string[],
+		'video:category'?: string,
+		'video:restriction'?: { '@relationship': 'allow' | 'deny', '#text': string },
+		'video:gallery_loc'?: { '@title'?: string, '#text': string },
+		'video:price'?: { '#text': number, '@currency': string, '@type'?: 'rent' | 'own', '@resolution'?: 'HD' | 'SD' }[],
+		'video:requires_subscription'?: string,
+		'video:uploader'?: { '#text': string, '@info'?: string },
+		'video:platform'?: { '@relationship': 'allow' | 'deny', '#text': string },
+		'video:live'?: string,
+	}[],
+};
+
+export type MappedNewsSiteMapData = {
+	loc: string,
+	'news:news': {
+		'news:publication': {
+			'news:name': string,
+			'news:language': string,
+		},
+		'news:genres'?: string | string[],
+		'news:publication_date': string,
+		'news:title': string,
+		'news:keywords'?: string | string[],
+		'news:stock_tickers'?: string | string[],
+	},
+};
