a123dfc18523d59ff401b6ec0280c49c75c5c506
Michael Thanei <Michael.Thanei@googlemail.com>
2018-09-20 09:30:07 +0200

    Refine error handling in RelayConnectionFetcher (#16)
    - retries on recoverable HTTP status codes, e.g. 429 or 503
    - passes GraphQL errors into `transformResult` to be optionally handled by client

--- src/fetching/RelayConnectionFetcher.js
+++ src/fetching/RelayConnectionFetcher.js
@@ -10,18 +10,24 @@ type Props<T, S> = FetcherProps<T, S> & {
 	query: string,
 	logErrors?: boolean,
 	maxRetries?: number,
+	retryCodes?: Array<number>;
+	exitCodes?: Array<number>;
 };
 
 type GetConnection<T> = <T>(x: Object) => GraphQlConnection<T>;
 type GraphQlConnection<T> = { edges: { node: T, cursor: string }[], pageInfo: { hasNextPage: boolean } };
+type GraphQlErrors = Array<{ message: string }>;
+type FetchResult<T> = { data: GraphQlConnection<T>, errors: ?GraphQlErrors };
 
 export default class RelayConnection<T, Data> implements Fetcher<Data[]> {
 	url: string;
 	query: string;
 	chunkSize: number;
-	transformResult: T => Data;
+	transformResult: (T, ?GraphQlErrors) => Data;
 	logErrors: boolean;
 	maxRetries: number;
+	retryCodes: Array<number>;
+	exitCodes: Array<number>;
 	getConnection: GetConnection<T>;
 	variables: {
 		first: number,
@@ -31,7 +37,15 @@ export default class RelayConnection<T, Data> implements Fetcher<Data[]> {
 	};
 
 	constructor({
-		url, getConnection, transformResult, query, chunkSize, logErrors, maxRetries,
+		url,
+		getConnection,
+		transformResult,
+		query,
+		chunkSize,
+		logErrors,
+		maxRetries,
+		retryCodes,
+		exitCodes,
 	}: Props<T, Data>) {
 		this.url = url;
 		this.getConnection = getConnection;
@@ -40,15 +54,31 @@ export default class RelayConnection<T, Data> implements Fetcher<Data[]> {
 		this.maxRetries = maxRetries || 3;
 		this.chunkSize = chunkSize || 100;
 		this.logErrors = logErrors || false;
+		/* eslint-disable line-comment-position */
+		this.retryCodes = retryCodes || [
+			408, // Request Timeout
+			429, // Too Many Requests
+			449, // The request should be retried after doing the appropriate action
+			499, // Client Closed Request
+			500, // Internal Server Error
+			502, // Bad Gateway
+			503, // Service Unavailable
+			504, // Gateway Timeout
+		];
+		this.exitCodes = exitCodes || [
+			400, // Bad Request
+			404, // Not Found
+		];
+		/* eslint-enable line-comment-position */
 	}
 
 	async* getData(): AsyncGenerator<Data[], void, void> {
 		let hasNextPage;
 
 		do {
-			const data: GraphQlConnection<T> = await this.fetchConnection();
+			const { data, errors } = await this.fetchConnection();
 
-			yield data.edges.map(edge => this.transformResult(edge.node));
+			yield data.edges.map(edge => this.transformResult(edge.node, errors));
 			({ hasNextPage } = data.pageInfo);
 
 			this.variables = {
@@ -58,9 +88,9 @@ export default class RelayConnection<T, Data> implements Fetcher<Data[]> {
 		} while (hasNextPage);
 	}
 
-	async fetchConnection(): Promise<GraphQlConnection<T>> {
+	async fetchConnection(): Promise<FetchResult<T>> {
 		const {
-			url, query, variables, getConnection, logErrors,
+			url, query, variables, getConnection, logErrors, retryCodes, exitCodes,
 		} = this;
 		let retries = 0;
 		const options = {
@@ -72,22 +102,30 @@ export default class RelayConnection<T, Data> implements Fetcher<Data[]> {
 		};
 
 		while (retries < this.maxRetries) {
-			const res = await fetch(url, options);
-			const data = await res.json();
-
-			if (data.errors && Array.isArray(data.errors)) {
+			try {
+				const res = await fetch(url, options);
+				const { status } = res;
+				if (retryCodes.includes(status)) {
+					retries = retries + 1;
+					if (logErrors) {
+						// eslint-disable-next-line no-console
+						console.log(`RelayConnectionFetcher: retry after server response code ${status}`);
+					}
+				} else if (exitCodes.includes(status)) {
+					throw new Error(`server response error ${status}`);
+				} else {
+					const data = await res.json();
+					return {
+						data: getConnection(data),
+						errors: data.errors,
+					};
+				}
+			} catch (error) {
 				if (logErrors) {
 					// eslint-disable-next-line no-console
-					console.log(
-						`RelayConnectionFetcher: Received errors in connection:\n"${data.errors
-							.map(err => (err.message ? err.message : '<no error message provided>'))
-							.join('\n')}"`
-					);
+					console.log('RelayConnectionFetcher: Received major error while fetching connection: ', error);
 				}
-
-				retries = retries + 1;
-			} else {
-				return getConnection(data);
+				throw error;
 			}
 		}
 
